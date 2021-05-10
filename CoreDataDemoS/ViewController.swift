59865e5044917f07235207afc5190de3419d0747
Alexey Barinov <begoodbebad2012@gmail.com>
2021-05-10 20:07:01 +0300

    Initial Commit

---
+++ CoreDataDemoS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemoS
+//
+//  Created by Mac on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
